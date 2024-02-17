fa077f9c4a25c9d3c391fc55b77ae2bb549ebff7
DuDy4 <danshevel@gmail.com>
2024-02-17 23:55:40 +0200

    Auth provider fix

--- src/providers/AuthProvider.js
+++ src/providers/AuthProvider.js
@@ -1,17 +1,17 @@
 import {createContext, useEffect, useState} from "react";
 import useFetch from "../hooks/useFetch";
 
-export const AuthContext = createContext(null);
+export const AuthContext = createContext(undefined);
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [userToken, setUserToken] = useState('')
-  const url = "http://localhost:4000/oauth";
+  const url = process.env.REACT_APP_API_SERVER_URL + "/oauth";
   const {handleGoogle} = useFetch(url)
 
   const signIn = (user) => {
     setUser(user)
-    localStorage.setItem("user", user.given_name)
+    localStorage.setItem("user", user)
   }
 
   const signOut = () => {
@@ -27,14 +27,14 @@ export function AuthProvider({ children }) {
   }
 
   useEffect(() => {
-    const theUser = localStorage.getItem("user");
-    const theToken = localStorage.getItem("token")
-
-    if (theUser && !theUser.includes("undefined") &&
-        theToken && !theToken.includes(undefined)) {
-      setUser(JSON.parse(theUser));
-      setUserToken(JSON.parse(theToken))
-    } else {
+    const localStorageUser = localStorage.getItem("user");
+    const localStorageToken = localStorage.getItem("token")
+    if(!userToken) {
+      setUserToken(JSON.parse(localStorageToken));
+    }
+    if(!user) {
+      setUser(JSON.parse(localStorageUser));
+      //Start to render the login
       window.google?.accounts.id.initialize({
         client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
         callback: handleGoogle,
@@ -43,8 +43,8 @@ export function AuthProvider({ children }) {
           document.getElementById('signDiv'),
           { theme: 'outline', size: 'large' }
       );
-    }
-  }, [handleGoogle]);
+      }
+    }, [handleGoogle]);
 
   return (
       <AuthContext.Provider value={value}>
